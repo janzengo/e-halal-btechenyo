a61c4e95202118f9b34822ab69b119d1db5bd74c
Janzen Go <30427833+janzengo@users.noreply.github.com>
2024-05-15 14:39:11 +0800

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2024 <a href="https://btech.edu.ph">Dalubhsaang Politekniko ng Lungsod ng Baliwag</a></strong>
+</footer>
